01b6acde38e7308028b26a87cc08f4646312f914
moustafa12465 <konafa2121@yandex.com>
2019-08-03 20:17:21 +0200

    xxxx

--- app.js
+++ app.js
@@ -14,22 +14,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 //online datbase settings 
 const client = new pg.Client({
-    user: 'ieqesefgnvpaii',
-    host: 'ec2-23-21-177-102.compute-1.amazonaws.com',
-    database: 'd11jnjirlnrsc1',
-    password: '5b446d3cae94f6a4de9fe5115a95b803ebc3e882192404d35abf213a40c487ad',
+    user: 'bxvrblkzegbayy',
+    host: 'ec2-50-19-221-38.compute-1.amazonaws.com',
+    database: 'dafsqupq0ib8ok',
+    password: '33a34082ee00699f917c4467dd7fc10d7c45cfecb03bb8e672975b839245eb4b',
     port: 5432,
     ssl:true
 });
 //connect to database
 client.connect();
 //database cmd
-client.query("create table index (id serial,name text,phone text,time timestamp default now())");
+client.query("create table index (id serial,name text,phone text)");
 // ##########################################################################################################################################
 //
 app.get('/', function (req, res) { res.render('home') });
 app.get('/signup', function (req, res) { res.render('sign_up') });
-app.get('/add', function (req, res) { res.render('add') });
+app.get('/add', function (req, res) { res.render('add',{isadded:1}) });
 app.get('/signin', function (req, res) { res.render('sign_in') });
 app.get('/search', function (req, res) { res.render('search') });
 // ##########################################################################################################################################
@@ -44,11 +44,19 @@ app.post('/sigin',function(req,res){
 app.post('/add',function(req,res){
     var name = req.body.name;
     var phone = req.body.phone;
-client.query("INSERT INTO index (name,phone) VALUES ("+name+","+phone+")",function(err,res){
-    if(!err){res.render('add')}
+client.query("INSERT INTO index (name,phone) VALUES ('"+name+"','"+phone+"')",function(err,ress){
+   if(err){ res.render('add',{isadded:3})}else{res.render('add',{isadded:2})}
+
+
 })
 
-    console.log(req.body);
+})
+
+app.post('/search',function(req,res){
+var name = req.body.search;
+client.query("SELECT * FROM index WHERE name like '"+name+"%' limit 10",function(err,RES){
+    res.send(RES)
+})
 })
 // ##########################################################################################################################################
 app.listen(PORT, function () { console.log('Phone index started') });
